d90ea11de5a6ccc4f278e5ee043133a792d8dbf4
Heather Barr <hnbarr@yahoo.com>
2019-05-09 20:01:39 -0500

    done

---
+++ src/modules/divideNumbers.js
@@ -0,0 +1,13 @@
+import divide from "./divide";
+
+export function divideTen(num) {
+  return divide(num,10);
+}
+
+export function divideFive(num) {
+  return divide(num, 5);
+}
+
+export function divideOne(num) {
+    return divide(num, 1)
+}
